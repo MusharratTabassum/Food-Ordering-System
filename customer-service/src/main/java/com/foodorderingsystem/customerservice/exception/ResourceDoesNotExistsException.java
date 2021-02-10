268d6b8b4bbe640e1da72e4fe193f259c3aa99a6
MusharratTabassum <39344758+MusharratTabassum@users.noreply.github.com>
2021-02-11 02:51:57 +0600

    Delete customer-service directory

--- customer-service/src/main/java/com/foodorderingsystem/customerservice/exception/ResourceDoesNotExistsException.java
+++
@@ -1,7 +0,0 @@
-package com.foodorderingsystem.customerservice.exception;
-
-public class ResourceDoesNotExistsException extends Exception{
-    public ResourceDoesNotExistsException() {
-        super("Doesn't Exist!");
-    }
-}
--- customer-service/src/main/java/com/foodorderingsystem/customerservice/repository/CustomerRepository.java
+++
@@ -1,6 +0,0 @@
-package com.foodorderingsystem.customerservice.repository;
-
-import com.foodorderingsystem.customerservice.model.Customer;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface CustomerRepository extends MongoRepository<Customer, Integer> {}
