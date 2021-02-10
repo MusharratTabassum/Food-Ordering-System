19dcdc3f80d95f2fc195e03d33065980542d68f4
MusharratTabassum <39344758+MusharratTabassum@users.noreply.github.com>
2021-02-11 02:36:36 +0600

    Delete customer-service directory

--- customer-service/src/main/java/com/foodorderingsystem/customerservice/exception/ResourceAlreadyExistsException.java
+++
@@ -1,7 +0,0 @@
-package com.foodorderingsystem.customerservice.exception;
-
-public class ResourceAlreadyExistsException extends Exception {
-    public ResourceAlreadyExistsException(String resourse) {
-        super(resourse + "already Exists!");
-    }
-}
